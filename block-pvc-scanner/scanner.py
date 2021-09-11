a64b6cb4f91367064149ffaab99a5a1b2d624706
kais271 <sunkai5210770@live.com>
2021-09-11 15:16:10 +0800

    add logging and optimize some codes

--- block-pvc-scanner/scanner.py
+++ block-pvc-scanner/scanner.py
@@ -4,7 +4,7 @@ import time
 import logging
 
 from prometheus_client import start_http_server, Gauge
-g=Gauge('pvc_usage','fetching pvc usge matched by k8s csi',['volumename'])
+g=Gauge('pv_usage','fetching pvc usge matched by k8s csi',['volumename'])
 #set metrics
 start_http_server(8848)
 
@@ -15,26 +15,32 @@ print_log = logging.StreamHandler()
 print_log.setFormatter(formatter)
 logger.addHandler(print_log)
 
+def get_pv_usage(pvc_info):
+  for pv_usage in pvc_info:
+    #get pvc usgae
+    if re.match("^[0-9]*\%",pv_usage):
+      pv_usage=float(pv_usage.strip('%'))/100.0
+      logger.info(f'VOLUME: {pv}, USAGE: {pv_usage}')
+      g.labels(pv).set(pv_usage)
+  return 
+
 while 1:
   get_pvc=os.popen("df -h|grep -E 'kubernetes.io/flexvolume|kubernetes.io~csi|kubernetes.io/gce-pd/mounts'")
   all_pvcs=get_pvc.readlines()
   if(len(all_pvcs) == 0):
-    logger.warning("Not block storage pvc found or not supported yet.")
-  for pvc in all_pvcs:
-    #get pvc name
-    pvc_info=pvc.split(' ')
-    volume=pvc_info[-1].split('/')[-1]
-    for gke_pvc in pvc_info[-1].split('/'):
-      if re.match("^pvc",gke_pvc):
-        volume=gke_pvc
-      elif re.match("^gke-data",gke_pvc):
-        volume='pvc'+gke_pvc.split('pvc')[-1]
-     
-    for pvc_usage in pvc_info:
-      #get pvc usgae
-      if re.match("^[0-9]*\%",pvc_usage):
-        pvc_usage=float(pvc_usage.strip('%'))/100.0
-        logger.info(f'VOLUME: {volume}, USAGE: {pvc_usage}')
-        g.labels(volume).set(pvc_usage)
-
+    logger.warning("No block storage pvc found or not supported yet.")
+  else:
+    for pvc in all_pvcs:
+      #get pvc name
+      pvc_info=pvc.split(' ')
+      volume=pvc_info[-1].split('/')[-1]
+      if re.match("^pvc",volume):
+        pv=volume
+        get_pv_usage(pvc_info)
+      elif re.match("^gke-data",volume):
+        pv='pvc'+volume.split('pvc')[-1]
+        get_pv_usage(pvc_info)
+      else:
+        logger.error(f'Canot match this volume: {volume}')
+  logger.info("Will sleep 15s...")
   time.sleep(15)
