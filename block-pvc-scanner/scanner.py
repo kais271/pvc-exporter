599890bbfa0777284017334f8e16394185d60bab
kai <sunkai5210770@live.com>
2021-09-11 13:12:33 +0800

    add logging

--- block-pvc-scanner/scanner.py
+++ block-pvc-scanner/scanner.py
@@ -1,14 +1,25 @@
-import os,re,time
+import os
+import re
+import time
+import logging
+
 from prometheus_client import start_http_server, Gauge
 g=Gauge('pvc_usage','fetching pvc usge matched by k8s csi',['volumename'])
 #set metrics
 start_http_server(8848)
 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('block_pvc_scanner')
+logger.setLevel(logging.DEBUG)
+print_log = logging.StreamHandler()
+print_log.setFormatter(formatter)
+logger.addHandler(print_log)
+
 while 1:
   get_pvc=os.popen("df -h|grep -E 'kubernetes.io/flexvolume|kubernetes.io~csi|kubernetes.io/gce-pd/mounts'")
   all_pvcs=get_pvc.readlines()
   if(len(all_pvcs) == 0):
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), 'Warning: Not found block storage pvc.Or not supported yet.')
+    logger.warning("Not block storage pvc found or not supported yet.")
   for pvc in all_pvcs:
     #get pvc name
     pvc_info=pvc.split(' ')
@@ -23,7 +34,7 @@ while 1:
       #get pvc usgae
       if re.match("^[0-9]*\%",pvc_usage):
         pvc_usage=float(pvc_usage.strip('%'))/100.0
-        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), volume, pvc_usage)
+        logger.info(f'VOLUME: {volume}, USAGE: {pvc_usage}')
         g.labels(volume).set(pvc_usage)
 
   time.sleep(15)
